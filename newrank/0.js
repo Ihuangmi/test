c46bb81cd7f8b0ae505c1531ec8034dbd9c551a0
huangmi <huangmi@newrank.cn>
2023-08-09 17:07:57 +0800

    test

--- newrank/0.js
+++ newrank/0.js
@@ -1,3 +1,101 @@
-// 000100000001867eb78e6e7e34b928dd65759e0efb44f49bef1627a4fbccd1043e9fb0b71bc21778f696cb4d8b58
-// 000100000001036d889a5eea8b860b0f930f529f9c208b5404e064066ddc9df3cfc623e0d4461778f791cd2a1a03
-// 000100000001d9dc21ee78c8949d3666b262b56d22963f92bb9c1f14e10421796fdd200215a61778f7be3a4a6786
+(e.GetTimeStamp = function (e) {
+  return e ? Date.now().toString().substring(0, e) : Date.now().toString();
+}),
+  (e.GetNowDate = function () {
+    return new Date();
+  }),
+  (e.GetTimeStampByDate = function (e, t) {
+    return t ? e.getTime().toString().substring(0, t) : e.getTime().toString();
+  }),
+  (e.TimeStampToDateTimeStr = function (e) {
+    if (!e) return "参数不能为空";
+    try {
+      return i.format(i.fromUnixTime(parseInt(e)), "yyyy-MM-dd HH:mm:ss");
+    } catch (t) {
+      return "非法时间戳";
+    }
+  }),
+  (e.Now = function () {
+    return i.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+  }),
+  (e.Today = function () {
+    return new Date(i.format(new Date(), "yyyy-MM-dd 00:00:00"));
+  }),
+  (e.AddZeroDay = function (e, t) {
+    void 0 === t && (t = "yyyy-MM-dd HH:mm:ss");
+    var n = new Date(i.format(new Date(), "yyyy-MM-dd 00:00:00"));
+    return n.setHours(n.getHours() + 24 * e), new Date(i.format(n, t));
+  }),
+  (e.TimeStampToDateTime = function (e) {
+    if (!e) throw "参数不能为空";
+    return i.fromUnixTime(parseInt(e));
+  }),
+  (e.GetChinaGMTDate = function () {
+    return new Date();
+  }),
+  (e.Parse = function (e) {
+    return new Date(e);
+  }),
+  (e.AddSeconds = function (e, t) {
+    void 0 === t && (t = "yyyy-MM-dd HH:mm:ss");
+    var n = new Date();
+    return n.setSeconds(n.getSeconds() + e), new Date(i.format(n, t));
+  }),
+  (e.AddMinutes = function (e, t) {
+    void 0 === t && (t = "yyyy-MM-dd HH:mm:ss");
+    var n = new Date();
+    return n.setMinutes(n.getMinutes() + e), new Date(i.format(n, t));
+  }),
+  (e.AddDays = function (e, t) {
+    void 0 === t && (t = "yyyy-MM-dd HH:mm:ss");
+    var n = new Date();
+    return n.setHours(n.getHours() + 24 * e), new Date(i.format(n, t));
+  }),
+  (e.AddDaysFrom = function (e, t, n) {
+    void 0 === n && (n = "yyyy-MM-dd HH:mm:ss");
+    var i = e;
+    return i.setHours(i.getHours() + 24 * t), i;
+  }),
+  (e.AddHours = function (e, t) {
+    void 0 === t && (t = "yyyy-MM-dd HH:mm:ss");
+    var n = new Date();
+    return n.setHours(n.getHours() + e), new Date(i.format(n, t));
+  }),
+  (e.FormartDateToStr = function (e, t) {
+    return void 0 === t && (t = "yyyy-MM-dd HH:mm:ss"), i.format(e, t);
+  }),
+  (e.AddMinutesString = function (e) {
+    return i.format(i.addMinutes(new Date(), e), "yyyy-MM-dd HH:mm:ss");
+  }),
+  (e.FormatString = function (e, t) {
+    return void 0 === t && (t = "yyyy-MM-dd HH:mm:ss"), i.format(e, t);
+  }),
+  (e.AddDaysFromOneDay = function (e, t, n) {
+    void 0 === n && (n = "yyyy-MM-dd HH:mm:ss");
+    var o = new Date(e);
+    return o.setHours(o.getHours() + 24 * t), new Date(i.format(o, n));
+  }),
+  (e.GetTicks = function (e) {
+    var t = (1e4 * new Date().getTime() + 621355968e9).toString();
+    if (
+      ((t = t.substring(0, t.length - 4)),
+      (t += Math.ceil(1e4 * Math.random()).toString()),
+      t.length > e)
+    )
+      return t.substring(0, e);
+    for (var n = e - t.length, i = "", o = 0; o < n; o++)
+      i += Math.ceil(10 * Math.random()).toString();
+    return t + i;
+  }),
+  (e.IsBefore = function (e, t) {
+    return i.isBefore(e, t);
+  }),
+  (e.IsSameDay = function (e, t) {
+    return i.isSameDay(e, t);
+  });
+
+Oe["effectiveTime"] = parseInt(
+  o.DateHelper.GetTimeStampByDate(o.DateHelper.Parse(Ee), 10)
+);
+
+new Date(e);
